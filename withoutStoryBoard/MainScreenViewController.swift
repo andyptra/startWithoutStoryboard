1c1cba1ffc5f31a243399f1094bba85ee601b57a
Andy saputra <andygrap@gmail.com>
2020-05-04 23:08:20 +0700

    first commit

---
+++ withoutStoryBoard/MainScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreenViewController.swift
+//  withoutStoryBoard
+//
+//  Created by andyptra on 5/4/20.
+//  Copyright © 2020 andyptra. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
