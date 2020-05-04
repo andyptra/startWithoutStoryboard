0f7b6bc3cd3ff8d955f27b7641261f0990d34cfb
Andy saputra <andygrap@gmail.com>
2020-05-04 22:59:41 +0700

    Initial Commit

---
+++ withoutStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  withoutStoryBoard
+//
+//  Created by andyptra on 5/4/20.
+//  Copyright © 2020 andyptra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
